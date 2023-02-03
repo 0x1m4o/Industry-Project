37359482cfb16f4276e468949de8694b9f8668dd
Iyan Siagian <iyansiagian04@gmail.com>
2023-02-03 14:25:51 +0700

    footer responsive1

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,10 +1,11 @@
 <!-- {Links} -->
+<link rel="stylesheet" href="css/footer.css">
 <div class="container">
     <hr>
     <div class="row d-flex mx-5">
         <div class="col-5 p-0">
             <img src="img/itc.png" alt="" class="footer-logo">
-            <h4>Information And Technology Club</h4>
+            <h4 class="jurusan">Information And Technology Club</h4>
             <p class="text-center ms-3 mt-3" style="font-size: 14px; width:300px;">Jl. Swadaya, RT.7/RW.7, Jatinegara,
                 Kec. Cakung, Kota Jakarta Timur, Daerah Khusus Ibukota Jakarta 13930</p>
 
@@ -41,10 +42,7 @@
         </div>
 
         <div class="col-7 p-0">
-            <iframe
-                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15865.73276124494!2d106.925874!3d-6.2064508!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e698bcabb1368d7%3A0xea46dd080cc5e54c!2sSMKN%2069%20JAKARTA!5e0!3m2!1sid!2sid!4v1675236027934!5m2!1sid!2sid"
-                width="650" height="400" style="border:0;" allowfullscreen="" loading="lazy"
-                referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15865.73276124494!2d106.925874!3d-6.2064508!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e698bcabb1368d7%3A0xea46dd080cc5e54c!2sSMKN%2069%20JAKARTA!5e0!3m2!1sid!2sid!4v1675236027934!5m2!1sid!2sid" width="650" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
     </div>
 
