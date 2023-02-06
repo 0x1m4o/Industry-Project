cf10292ece40497cf6db28aa5caf3f0d7cbfb95b
Iyan Siagian <iyansiagian04@gmail.com>
2023-02-06 14:06:55 +0700

    partner responsive1

---
+++ resources/views/partials/partner.blade.php
@@ -0,0 +1,45 @@
+<link rel="stylesheet" href="css/partner.css">
+
+<h1 class="text-center mb-4 fw-bolder"> Partner </h1>
+
+<div class="container partner mb-5 grid">
+        <div class="card d-flex justify-content-center">
+            <div class="d-flex align-items-center img-center p-1">
+                <img class="card-img-top partner-logo" src="img/google.svg" alt="Card image cap">
+
+            </div>
+        </div>
+
+        <div class="card d-flex justify-content-center">
+            <div class="d-flex align-items-center img-center p-1">
+                <img class="card-img-top partner-logo" src="img/microsoft.svg" alt="Card image cap">
+            </div>
+        </div>
+
+
+        <div class="card d-flex justify-content-center">
+            <div class="d-flex align-items-center img-center p-1">
+                <img class="card-img-top partner-logo" src="img/apple.svg" alt="Card image cap">
+            </div>
+        </div>
+
+        <div class="card d-flex justify-content-center">
+            <div class="d-flex align-items-center img-center p-1">
+                <img class="card-img-top partner-logo" src="img/nasa.svg" alt="Card image cap">
+            </div>
+        </div>
+
+
+        <div class="card d-flex justify-content-center">
+            <div class="d-flex align-items-center img-center p-1">
+                <img class="card-img-top partner-logo" src="img/netflix.svg" alt="Card image cap">
+            </div>
+        </div>
+
+        <div class="card d-flex justify-content-center">
+            <div class="d-flex align-items-center img-center p-1">
+                <img class="card-img-top partner-logo" src="img/amazon.svg" alt="Card image cap">
+            </div>
+        </div>
+
+</div>
--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.main')
 
 @section('content')
-    <h1 class="text-center m-5">Test</h1>
+<h1 class="text-center m-5">Test</h1>
+
+@include("partials.partner")
 @endsection
