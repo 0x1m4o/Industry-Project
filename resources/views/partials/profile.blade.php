8d92438f9a4b43a076d47301ca82836fdaab5820
ridwanalfarezi <alfareziridwan@gmail.com>
2023-03-01 10:55:12 +0700

    homepage complete

--- resources/views/partials/profile.blade.php
+++ resources/views/partials/profile.blade.php
@@ -1,22 +1,18 @@
 <link rel="stylesheet" href="css/profile.css">
-<div class="container">
-    <div class="row row1 my-5">
-        <div class="col-5">
-            <img class="ilham" src="/img/programmings.jpg" alt="">
-
+<div class="container" style="margin: 100px auto;">
+    <div class="row gap-3">
+        <div class="col-md-5">
+            <img class="w-100 rounded-3" src="/img/pp.jpg">
         </div>
 
-        <div class="col-6 col-width">
+        <div class="col-md-6 col-width">
             <div class="td container-fluid p-0">
-                <h1 class="pro-title mb-3 "> Pemrograman Pelatihan</h1>
-
-                <p class="pro-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi provident quis ipsa assumenda nihil molestias, sed ullam expedita quibusdam voluptas id vero ad temporibus beatae, rerum earum dolorem quaerat quia sit adipisci ea voluptatibus esse? Atque rerum magni vero voluptas, esse omnis corporis totam, ad earum ratione officia impedit consequatur.</p>
-
+                <h1 class="pro-title mb-3 ">Program Pelatihan</h1>
+                <p class="pro-desc">Program pelatihan kami didesain untuk membantu peserta meningkatkan keterampilan dan pengetahuan mereka dalam berbagai bidang karir. Kami menawarkan program pelatihan yang terstruktur dengan baik, lengkap dengan fasilitas terbaik dan didukung oleh tim ahli pelatih yang berpengalaman. Program kami dirancang untuk memenuhi kebutuhan karir saat ini dan membantu peserta dalam mengembangkan keterampilan yang diperlukan di masa depan.
+                </p>
             </div>
-            <div class="container-fluid btn-kunjungi">
-                <button class="btn-kunjungi2">
-                    <p class="btn-to"> Kunjungi</p>
-                </button>
+            <div class="container-fluid btn-kunjungi p-0">
+                <button type="submit" class="btn btn-lg btn-primary shadow-btn px-5 rounded-pill" style="background-color: #187BCD">Selengkapnya</button>
             </div>
         </div>
     </div>
