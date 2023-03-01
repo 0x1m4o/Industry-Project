62842f1bd59ff7eae3b62962dfbd4d7c4107e4b8
Iyan Siagian <iyansiagian04@gmail.com>
2023-03-01 11:20:40 +0700

    nambahin a

--- resources/views/user/program.blade.php
+++ resources/views/user/program.blade.php
@@ -17,7 +17,7 @@
     }
 
     div.border:hover {
-        background-color: #eeeeee; 
+        background-color: #eeeeee;
         transition: all .1s linear;
     }
 
@@ -46,7 +46,7 @@
         <div class="d-flex justify-content-end">
             <div class="dropdown">
                 <button class="btn btn-lg btn-primary shadow-btn rounded-pill dropdown-toggle" style="background-color: #187BCD" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Kategori
+                    Kategori
                 </button>
                 <ul class="dropdown-menu p-0">
                     <li><a class="dropdown-item kategori" href="#">Programming</a></li>
@@ -56,25 +56,28 @@
             </div>
         </div>
         @forelse ($programs as $program)
+        <a href="p">
         <div class="card" style="width: 25rem;">
-        <div class="d-flex">
-            <img src="{{ Storage::url('public/programs/').$program->gambar }}" class="card-img-top img-card" alt="...">
-            <div class="my-1 w-100">
-                <h4 class="prog-title">{{$program->nama}}</h4>
+            <div class="d-flex">
+                <img src="{{ Storage::url('public/programs/').$program->gambar }}" class="card-img-top img-card" alt="...">
+                <div class="my-1 w-100">
+                    <h4 class="prog-title">{{$program->nama}}</h4>
 
-                <div class="d-flex justify-content-center mt-1">
-                    <iconify-icon icon="iconoir:design-pencil" class="design-pen" width="25" height="25"></iconify-icon>
-                    <h5 class="prog-title1">{{$program->category}}</h5>
+                    <div class="d-flex justify-content-center mt-1">
+                        <iconify-icon icon="iconoir:design-pencil" class="design-pen" width="25" height="25"></iconify-icon>
+                        <h5 class="prog-title1">{{$program->category}}</h5>
+                    </div>
+
                 </div>
-
             </div>
+            <p class="desc">{!! substr($program->deskripsi, 0, 100) !!}</p>
         </div>
-        <p class="desc">{!! substr($program->deskripsi, 0, 100) !!}</p>
-    </div>
+        </a>
         @empty
         <div class="alert alert-danger">
             Data Program belum Tersedia.
         </div>
+        </a>
         @endforelse
     </div>
     <button class="btn btn-lg btn-primary shadow-btn rounded-pill d-block mx-auto my-5" style="background-color: #187BCD">
