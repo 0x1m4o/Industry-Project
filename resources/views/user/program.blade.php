5d8aaa9c8f441bea60a732021a4c6bbcb53afbc1
ridwanalfarezi <alfareziridwan@gmail.com>
2023-03-01 09:19:34 +0700

    Program Kelar Cuy

--- resources/views/user/program.blade.php
+++ resources/views/user/program.blade.php
@@ -19,10 +19,19 @@
         background-color: #eeeeee; 
         transition: all .1s linear;
     }
+
+    .kategori:active {
+        background-color: #187BCD !important;
+        color: white !important;
+    }
+
+    .shadow-btn:hover {
+        background-color: #1567aa !important;
+    }
 </style>
 
 
-<header class="py-5 mt-5" style="background: #f4f4f4">
+<header class="py-5" style="background: #f4f4f4">
     <div class="container px-lg-5 my-5">
         <div class="text-center text-dark">
             <h1 class="display-4 fw-bolder">Program</h1>
@@ -32,17 +41,19 @@
 </header>
 
 <div class="container p-0" style="margin-top: 100px;">
-    <div class="dropdown text-end">
-        <button class="btn btn-outline-dark dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-          Dropdown button
-        </button>
-        <ul class="dropdown-menu">
-          <li><a class="dropdown-item" href="#">Programming</a></li>
-          <li><a class="dropdown-item" href="#">Networking</a></li>
-          <li><a class="dropdown-item" href="#">Design</a></li>
-        </ul>
-      </div>
     <div class="row m-0 w-full g-2">
+        <div class="d-flex justify-content-end">
+            <div class="dropdown">
+                <button class="btn btn-lg btn-primary shadow-btn rounded-pill dropdown-toggle" style="background-color: #187BCD" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Kategori
+                </button>
+                <ul class="dropdown-menu p-0">
+                    <li><a class="dropdown-item kategori" href="#">Programming</a></li>
+                    <li><a class="dropdown-item kategori" href="#">Networking</a></li>
+                    <li><a class="dropdown-item kategori" href="#">Design</a></li>
+                </ul>
+            </div>
+        </div>
         @forelse ($programs as $program)
         <div class="col-lg-4 col-md-6">
             <a href="{{ route('user.show', $program->id) }}">
@@ -67,5 +78,8 @@
         </div>
         @endforelse
     </div>
+    <button class="btn btn-lg btn-primary shadow-btn rounded-pill d-block mx-auto my-5" style="background-color: #187BCD">
+        Muat Lebih Banyak
+    </button>
 </div>
 @endsection
