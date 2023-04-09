ad213a0282b47432aeefb9be4af2bc88cc132000
ridwanalfarezi <alfareziridwan@gmail.com>
2023-04-10 00:00:26 +0700

    fix: partner

--- resources/views/partials/partner.blade.php
+++ resources/views/partials/partner.blade.php
@@ -2,40 +2,39 @@
 
 <h1 class="text-center my-5 fw-bolder"> Partner </h1>
 
-<div class="container partner grid">
-        <div class="card d-flex justify-content-center">
-            <div class="d-flex align-items-center img-center p-1">
+<div class="row justify-content-evenly my-5">
+        <div class="card d-flex justify-content-center col-lg-2 mb-lg-0 mb-5">
+            <div class="d-flex align-items-center justify-content-center">
                 <img class="card-img-top partner-logo" src="img/google.svg" alt="Card image cap">
-
             </div>
         </div>
 
-        <div class="card d-flex justify-content-center">
-            <div class="d-flex align-items-center img-center p-1">
+        <div class="card d-flex justify-content-center col-lg-2 mb-lg-0 mb-5">
+            <div class="d-flex align-items-center justify-content-center">
                 <img class="card-img-top partner-logo" src="img/microsoft.svg" alt="Card image cap">
             </div>
         </div>
 
-        <div class="card d-flex justify-content-center">
-            <div class="d-flex align-items-center img-center p-1">
+        <div class="card d-flex justify-content-center col-lg-2 mb-lg-0 mb-5">
+            <div class="d-flex align-items-center justify-content-center">
                 <img class="card-img-top partner-logo" src="img/apple.svg" alt="Card image cap">
             </div>
         </div>
 
-        <div class="card d-flex justify-content-center">
-            <div class="d-flex align-items-center img-center p-1">
+        <div class="card d-flex justify-content-center col-lg-2 mb-lg-0 mb-5">
+            <div class="d-flex align-items-center justify-content-center">
                 <img class="card-img-top partner-logo" src="img/tesla.svg" alt="Card image cap">
             </div>
         </div>
 
-        <div class="card d-flex justify-content-center">
-            <div class="d-flex align-items-center img-center p-1">
+        <div class="card d-flex justify-content-center col-lg-2 mb-lg-0 mb-5">
+            <div class="d-flex align-items-center justify-content-center">
                 <img class="card-img-top partner-logo" src="img/netflix.svg" alt="Card image cap">
             </div>
         </div>
 
-        <div class="card d-flex justify-content-center">
-            <div class="d-flex align-items-center img-center p-1">
+        <div class="card d-flex justify-content-center col-lg-2 mb-lg-0">
+            <div class="d-flex align-items-center justify-content-center">
                 <img class="card-img-top partner-logo" src="img/amazon.svg" alt="Card image cap">
             </div>
         </div>
